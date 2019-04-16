d9c0f6daff28e97731c7a81366f4819601d0a4e3
José Ricardo <zevatron@zevatron.com.br>
2019-04-16 08:43:17 -0300

    deploy

---
+++ precache-manifest.5dc98755895475c2fadb6144f07f6f6e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "25c2d49dcb779a63a6cf77ccf72ec8a8",
+    "url": "/desafio-redesoft/img/Icon home.25c2d49d.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/desafio-redesoft/robots.txt"
+  },
+  {
+    "revision": "d26ad9b6b2d9f23f76dc",
+    "url": "/desafio-redesoft/js/chunk-vendors.5101329d.js"
+  },
+  {
+    "revision": "f130fbe9ec2435909b65",
+    "url": "/desafio-redesoft/js/app.de93ec42.js"
+  },
+  {
+    "revision": "ed3c4660ec2fdd85d73b3e57d5bd1e8b",
+    "url": "/desafio-redesoft/index.html"
+  },
+  {
+    "revision": "f60c51d98e2aa732f87c8606f770e991",
+    "url": "/desafio-redesoft/img/posto.f60c51d9.jpg"
+  },
+  {
+    "revision": "21928608207af3340d622393a4775e76",
+    "url": "/desafio-redesoft/img/Icon promocoes.21928608.svg"
+  },
+  {
+    "revision": "f893ea1556c02f933261531d76589d70",
+    "url": "/desafio-redesoft/img/Icon postos.f893ea15.svg"
+  },
+  {
+    "revision": "ed9add013008a3b3ece2716354956640",
+    "url": "/desafio-redesoft/img/Icon menu.ed9add01.svg"
+  },
+  {
+    "revision": "c43a895c835d48401ecc0b6b152b5af7",
+    "url": "/desafio-redesoft/img/Icon extrato.c43a895c.svg"
+  },
+  {
+    "revision": "f130fbe9ec2435909b65",
+    "url": "/desafio-redesoft/css/app.9f7e6be6.css"
+  }
+];
